73cb74acf3b145a703a098cf760252dac40e82fa
Andrej Pavesic <apavesic22@foi.hr>
2025-12-29 21:06:13 +0100

    Added initial files

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
+})
+export class App {
+  title = 'Quizify frontend running';
+}
