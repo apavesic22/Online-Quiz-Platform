60514658e544b3d67a5051f2662437df83d95527
Andrej Pavesic <apavesic22@foi.hr>
2025-12-29 21:50:15 +0100

    Reconstructed home page. Tested soem buttons if they work

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss'],
-  imports: [RouterOutlet]
+  styleUrls: ['./app.scss']
 })
 export class App {
-  title = 'Quizify frontend running';
+  title = 'Quizify';
+
+  // TEMPORARY (will be replaced by AuthService later)
+  isLoggedIn = false;
+
+  onLogin() {
+    // later: open login dialog
+    console.log('Login clicked');
+  }
+
+  onLogout() {
+    // later: logout via AuthService
+    console.log('Logout clicked');
+  }
 }
